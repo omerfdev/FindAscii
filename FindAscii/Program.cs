8c641c8d21e546dced85de58bc5019f599b95d37
omeralmali1 <omeralmali1@gmail.com>
2023-04-15 20:40:38 +0300

    Add project files.

---
+++ FindAscii/Program.cs
@@ -0,0 +1,13 @@
+﻿char entry;
+Console.WriteLine("Please Enter Character");
+bool character=char.TryParse(Console.ReadLine(),out entry);
+if (character == true)
+{   
+    int ascii=((int)entry);
+    Console.WriteLine("Ascii Number:"+ascii);
+}
+else
+{
+    Console.WriteLine("Wrong");
+}
+Console.ReadLine();
