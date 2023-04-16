c8f803e2e2b25195c6e6b8a2c1439936090fb09b
omeralmali1 <omeralmali1@gmail.com>
2023-04-16 22:39:31 +0300

    Fixed Binary Code.

--- FindAscii/Program.cs
+++ FindAscii/Program.cs
@@ -5,6 +5,19 @@ if (character == true)
 {   
     int ascii=((int)entry);
     Console.WriteLine("Ascii Number:"+ascii);
+    
+    double binarycode = 0;
+    int i = 0;
+
+    while (ascii >= 2)
+    {
+        binarycode = binarycode + (ascii % 2) * (Math.Pow(10, i));
+        ascii = ascii / 2;
+        i++;
+    }
+    binarycode = binarycode + (ascii * Math.Pow(10, i));
+    Console.WriteLine(binarycode);
+    Console.ReadLine();
 }
 else
 {
